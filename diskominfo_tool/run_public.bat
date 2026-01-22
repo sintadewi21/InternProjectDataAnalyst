fb0f56f1424c49a7273ee2923eeb2cfaf58fe597
sintadewi21 <162396288+sintadewi21@users.noreply.github.com>
2026-01-22 10:57:52 +0700

    Delete diskominfo_tool directory

--- diskominfo_tool/run_public.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Menginstal dependensi baru (pyngrok)...
-pip install -r requirements.txt
-echo.
-echo Menjalankan aplikasi dengan akses Publik...
-echo Tekan Ctrl+C untuk berhenti.
-python run_public.py
-pause
